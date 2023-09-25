44e0306b12ce79ca676f2cd7264fbb5700ed2d15
王继浩 <wangjihaowjho@163.com>
2023-09-25 14:50:44 +0800

    vite项目配置

---
+++ vite-project/src/App.vue
@@ -0,0 +1,29 @@
+
+
+<template>
+  <div :id = "id">
+    掩饰v-bind
+  </div>
+</template
+
+<script setup lang="ts">
+const id:string = '123'
+
+
+
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
